d222ea1fe7b8d3f7be53ded9788a142f4d571331
meshari900 <107339420+meshari900@users.noreply.github.com>
2022-06-20 08:36:57 +0300

    Update saudiexchange.py

--- saudiexchange.py
+++ saudiexchange.py
@@ -9,12 +9,30 @@ soup = BeautifulSoup(data, 'html.parser')
 table = soup.find('table', class_='display')
 name = []
 Price = []
+vol = []
+chv = []
+chp = []
+nootc = []
+vtc = []
+bp = []
+bv = []
+bop = []
+bov = []
 for row in table.tbody.find_all('tr'):    
     columns = row.find_all('td')
     name.append(columns[1].text.strip())
     Price.append(columns[2].text.strip())
-list_of_tuple= list(zip(name, Price))
-df = pd.DataFrame(list_of_tuple,columns=['أسم الشركة', 'السعر'])
+    vol.append(columns[3].text.strip())
+    chv.append(columns[4].text.strip())
+    chp.append(columns[5].text.strip())
+    nootc.append(columns[6].text.strip())
+    vtc.append(columns[7].text.strip())
+    bp.append(columns[8].text.strip())
+    bv.append(columns[9].text.strip())
+    bop.append(columns[10].text.strip())
+    bov.append(columns[11].text.strip())
+list_of_tuple= list(zip(name, Price,vol,chv,chp,nootc,vtc,bp,bv,bop,bov))
+df = pd.DataFrame(list_of_tuple,columns=['أسم الشركة', 'السعر','آخر كمية','تغيير القيمة','التغير','تراكمي عدد الصفقات','تراكمي الكمية','أفضل طلب سعر','أفضل طلب كمية','أفضل عرض سعر','أفضل عرض كمية'])
 pd.set_option("display.max_rows", None)
 while True:
     now = datetime.datetime.now()
